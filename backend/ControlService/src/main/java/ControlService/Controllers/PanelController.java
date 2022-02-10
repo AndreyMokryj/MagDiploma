d5fc39b7b1e5ed07fc713a6783ae60fa1752b3bd
andrew <andrew_mokryj@ukr.net>
2022-02-11 00:36:29 +0200

    refactor: remove unused state components from ControlService

--- backend/ControlService/src/main/java/ControlService/Controllers/PanelController.java
+++ backend/ControlService/src/main/java/ControlService/Controllers/PanelController.java
@@ -1,18 +1,13 @@
 package ControlService.Controllers;
 
 import ControlService.Entities.PanelE;
-import ControlService.Entities.StateE;
 import ControlService.Repositories.PanelRepository;
-import ControlService.Repositories.StateRepository;
 import ControlService.vo.PanelVO;
-import ControlService.vo.StateVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping(path="/panels")
@@ -20,9 +15,6 @@ import java.util.UUID;
 public class PanelController {
     @Autowired
     private PanelRepository panelRepository;
-
-    @Autowired
-    private StateRepository stateRepository;
 
     @GetMapping(path="/")
     public @ResponseBody
@@ -49,62 +41,4 @@ public class PanelController {
         PanelE panel = PanelE.fromVO(panelVO);
         panelRepository.save(panel);
     }
-
-//    @GetMapping("/reduce/{panelId}")
-//    public void reduceForPanel(@PathVariable String panelId) {
-//        int k = 10;
-//
-//        List<StateE> states = (List<StateE>) stateRepository.findByPanelId(panelId);
-//        for (StateE state : states) {
-//            state.setAzPlus(state.getAzPlus() / k);
-//            state.setAzMinus(state.getAzMinus() / k);
-//            state.setAltPlus(state.getAltPlus() / k);
-//            state.setAltMinus(state.getAltMinus() / k);
-//        }
-//
-//        stateRepository.saveAll(states);
-//
-//    }
-
-//    private StateE getState(StateVO stateVO) {
-//        StateE state = StateE.fromVO(stateVO);
-//        try {
-//            Optional<StateE> state1 = stateRepository.findByParams(
-//                    state.getPanelId(),
-//                    state.getAzimuth(),
-//                    state.getAltitude()
-//            );
-//            return state1.get();
-//        }
-//        catch (Exception ex){
-//            state.setId(UUID.randomUUID().toString());
-//            StateE saved = stateRepository.save(state);
-//            return saved;
-//        }
-//    }
-
-//    @GetMapping("/prepare/{panelId}")
-//    public void preparePanel(@PathVariable String panelId) {
-//        PanelE panel = getById(panelId);
-//
-//        StateVO stateVO = new StateVO();
-//        stateVO.setPanelId(panelId);
-//        stateVO.setAzimuth(panel.getAzimuth());
-//        stateVO.setAltitude(panel.getAltitude());
-//
-//        StateE currentState = getState(stateVO);
-//        currentState.setAzPlus(0.00001);
-//        currentState.setAzMinus(0);
-//        currentState.setAltPlus(0);
-//        currentState.setAltMinus(0);
-//        stateRepository.save(currentState);
-//
-//        stateVO.setAzimuth((stateVO.getAzimuth() + 1) % 360);
-//        StateE nextState = getState(stateVO);
-//        nextState.setAzPlus(0);
-//        nextState.setAzMinus(0);
-//        nextState.setAltPlus(0);
-//        nextState.setAltMinus(0);
-//        stateRepository.save(nextState);
-//    }
 }
